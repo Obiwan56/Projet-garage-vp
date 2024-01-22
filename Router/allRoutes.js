8802505d1f4478b9037d93f41afd53ac609e61bc
Obiwan56 <mavrick_56@hotmail.com>
2024-01-22 19:35:37 +0100

    page presentation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/presentation", "Présentation", "/pages/presentation.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
