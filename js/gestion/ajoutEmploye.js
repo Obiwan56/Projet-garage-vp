d286c3e6abd09c2dd98443faec44cef86ae02ebc
Obiwan56 <mavrick_56@hotmail.com>
2024-02-19 11:33:34 +0100

    ajout employé ok

--- js/gestion/ajoutEmploye.js
+++ js/gestion/ajoutEmploye.js
@@ -101,3 +101,12 @@ $(document).ready(function() {
         });
     });
 });
+
+
+const togglePasswordBtn = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("pswEmpolye");
+
+togglePasswordBtn.addEventListener("click", function() {
+    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+});
